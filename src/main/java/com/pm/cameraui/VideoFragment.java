037d1e2a691df7b0097e72457c9ae2bbf95869db
zhenhong <565150953@qq.com>
2022-05-09 17:51:32 +0800

    摄像机工具

---
+++ src/main/java/com/pm/cameraui/VideoFragment.java
@@ -0,0 +1,190 @@
+package com.pm.cameraui;
+
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.SurfaceTexture;
+import android.os.Bundle;
+import android.util.Size;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+import com.pm.cameracore.DelegateCallback;
+import com.pm.cameracore.RecordDelegate;
+import com.pm.cameraui.widget.AutoFitTextureView;
+import com.pm.cameraui.widget.CameraController;
+import com.pm.cameraui.widget.CaptureButton;
+import com.pm.cameraui.widget.VideoViewController;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * @author pm
+ * @date 2019/9/17
+ * @email puming@zdsoft.cn
+ */
+public class VideoFragment extends BaseCameraFragment implements DelegateCallback {
+
+    private AutoFitTextureView mTextureView;
+    private VideoViewController mVideoViewController;
+    private CameraController mController;
+    private RecordDelegate mRecordDelegate;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment VideoFragment.
+     */
+    public static VideoFragment newInstance() {
+        VideoFragment fragment = new VideoFragment();
+        Bundle args = new Bundle();
+        // TODO: 2019/7/26
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public VideoFragment() {
+        super(true);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_video, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mTextureView = (AutoFitTextureView) view.findViewById(R.id.texture);
+        mController = view.findViewById(R.id.controller);
+        mVideoViewController = view.findViewById(R.id.vv_controller);
+        mRecordDelegate = new RecordDelegate(getActivity(), this);
+        mController.setTip("点击录制");
+        mController.setDuration(10);
+        mController.setAction(CaptureButton.Action.RECORD_VIDEO);
+        mController.setRecordPressMode(CaptureButton.PressMode.CLICK);
+        mController.setControllerCallback(new CameraController.ControllerCallback() {
+            @Override
+            public void takePicture() {
+            }
+
+            @Override
+            public void recordStart() {
+                mRecordDelegate.startRecordingVideo();
+            }
+
+            @Override
+            public void recordStop() {
+                mRecordDelegate.stopRecordingVideo(false);
+            }
+
+            @Override
+            public void onCancel() {
+                mVideoViewController.hide();
+            }
+
+            @Override
+            public void onConfirm() {
+                mVideoViewController.hide();
+            }
+
+            @Override
+            public void onClose() {
+                if (getActivity() != null) {
+                    getActivity().finishAfterTransition();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mRecordDelegate.startBackgroundThread();
+        if (mTextureView.isAvailable()) {
+            onPrepareCamera(mTextureView.getWidth(), mTextureView.getHeight());
+        } else {
+            mTextureView.setSurfaceTextureListener(this);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mRecordDelegate.stopRecordingVideo(true);
+        closeCamera();
+        mRecordDelegate.stopBackgroundThread();
+        mVideoViewController.hide();
+    }
+
+    @Override
+    protected void openCamera(int width, int height) {
+        mRecordDelegate.openCamera(width, height);
+    }
+
+    @Override
+    protected void closeCamera() {
+        mRecordDelegate.closeCamera();
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        onPrepareCamera(width, height);
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+
+    @Override
+    public void onChangeViewSize(Size size) {
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            mTextureView.setAspectRatio(size.getWidth(), size.getHeight());
+        } else {
+            mTextureView.setAspectRatio(size.getHeight(), size.getWidth());
+        }
+    }
+
+    @Override
+    public void onTransformView(Matrix matrix) {
+        mTextureView.setTransform(matrix);
+    }
+
+    @Override
+    public SurfaceTexture getSurfaceTexture() {
+        return mTextureView.getSurfaceTexture();
+    }
+
+    @Override
+    public void onCaptureResult(Bitmap bitmap) {
+
+    }
+
+    @Override
+    public void onRecordResult(Bitmap coverBitmap, String videoAbsolutePath) {
+        mVideoViewController.show(coverBitmap,videoAbsolutePath);
+    }
+}
